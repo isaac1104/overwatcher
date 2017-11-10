02993e925d775a52f6bbb7ecf3504c216d4e47f9
Isaac Kwon <you@example.com>
2017-11-09 17:36:29 -0800

    implemented bootstrap table for heroes stats

--- client/src/components/Data/HeroesData.js
+++ client/src/components/Data/HeroesData.js
@@ -8,82 +8,52 @@ const HeroesData = (props) => {
 
   console.log(sorted);
 
+  _.map(props.result.competitiveStats.topHeroes, (value, key) => {
+    console.log(key, value);
+  });
+
     return (
-    <div className="card">
-      <div className="card-body">
-        <div className="row">
-          <div className="col-md-3">
-            <div className="card-title">
-              <h5>Heroes</h5>
-              <hr/>
-              {_.map(props.result.competitiveStats.topHeroes, (value, key) => {
-                return (
-                  <div className="heroes">
+      <div>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Heroes</th>
+              <th>W</th>
+              <th>L</th>
+              <th>Win Rate</th>
+              <th>K/D</th>
+              <th>Time Played</th>
+            </tr>
+          </thead>
+          <tbody>
+            {_.map(props.result.competitiveStats.topHeroes, (value, key) => {
+              return(
+                <tr>
+                  <td className="heroes">
                     <img src={img} className="portrait" alt="portrait"/>
                     <h6>{key}</h6>
-                  </div>
-                )
-              })}
-            </div>
-          </div>
-          <div className="col-md-1">
-            <div className="card-title">
-              <h5>W</h5>
-              <hr/>
-              {_.map(props.result.competitiveStats.topHeroes, (value, key) => {
-                return (
-                  <h6>{value.gamesWon}</h6>
-                )
-              })}
-            </div>
-          </div>
-          <div className="col-md-1">
-            <div className="card-title">
-              <h5>L</h5>
-              <hr/>
-              {_.map(props.result.competitiveStats.topHeroes, (value, key) => {
-                return (
-                  <h6>{value.gamesPlayed}</h6>
-                )
-              })}
-            </div>
-          </div>
-          <div className="col-md-2">
-            <div className="card-title">
-              <h5>Win %</h5>
-              <hr/>
-              {_.map(props.result.competitiveStats.topHeroes, (value, key) => {
-                return (
-                  <h6>{value.winPercentage}%</h6>
-                )
-              })}
-            </div>
-          </div>
-          <div className="col-md-2">
-            <div className="card-title">
-              <h5>K/D</h5>
-              <hr/>
-              {_.map(props.result.competitiveStats.topHeroes, (value, key) => {
-                return (
-                  <h6>{value.eliminationsPerLife}</h6>
-                )
-              })}
-            </div>
-          </div>
-          <div className="col-md-3">
-            <div className="card-title">
-              <h5>Time Played</h5>
-              <hr/>
-              {_.map(props.result.competitiveStats.topHeroes, (value, key) => {
-                return (
-                  <h6>{value.timePlayed}</h6>
-                )
-              })}
-            </div>
-          </div>
-        </div>
+                  </td>
+                  <td>
+                    <h6>{value.gamesWon}</h6>
+                  </td>
+                  <td>
+                    <h6></h6>
+                  </td>
+                  <td>
+                    <h6>{value.winPercentage}</h6>
+                  </td>
+                  <td>
+                    <h6>{value.eliminationsPerLife}</h6>
+                  </td>
+                  <td>
+                    <h6>{value.timePlayed}</h6>
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
       </div>
-    </div>
   )
 };
 
