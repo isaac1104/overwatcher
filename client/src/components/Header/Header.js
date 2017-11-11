d319dda8c9ddf7976bc04bca11e662853993eb98
isaac1104 <isaac-kwon@hotmail.com>
2017-11-11 12:05:02 -0800

    error header styled

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -4,9 +4,13 @@ import "./Header.css";
 const Header = (props) => {
   if (props.playerFound === false) {
     return (
-      <div className="container error-header">
-        <div className="jumbotron">
-          <h1>Player not found</h1>
+      <div className="container">
+        <div className="jumbotron error-header text-center">
+          <h1>Error: Player Not Found!</h1>
+          <hr/>
+          <h3>Please check the BattleTag again or selected region for this player</h3>
+          <h4>The BattleTag must be in "battletag-number" format</h4>
+          <h5>Ex)sinatraa-11809</h5>
         </div>
       </div>
     )
