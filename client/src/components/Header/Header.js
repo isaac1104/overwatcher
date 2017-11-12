f99226e830192d829458519b1618f3534a310e86
isaac1104 <isaac-kwon@hotmail.com>
2017-11-11 21:38:58 -0800

    minor changes to header and header.css

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -9,7 +9,7 @@ const Header = (props) => {
           <h1>Error: Stats/Player Not Found!</h1>
           <hr/>
           <h3>Please check the BattleTag again or selected region for this player</h3>
-          <h4>The BattleTag must be in "battletag-number" format</h4>
+          <h4>The search must be in "battletag-number" format</h4>
           <h5>Ex)sinatraa-11809</h5>
         </div>
       </div>
