58557d57631a841e65229ec2ed984ddf2ed58669
isaac1104 <isaac-kwon@hotmail.com>
2017-11-14 15:15:41 -0800

    repositioned navbar

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -3,15 +3,17 @@ import "./Search.css";
 
 const Search = (props) => {
   return(
-    <form onSubmit={props.handleFormSubmit}>
-      <input onChange={props.handleInputChange} type="text" value={props.battleTag} id="search-box" autoComplete="off" placeholder="battletag-number"/>
-      <select value={props.region} onChange={props.handleSelectChange}>
-        <option value="us">US</option>
-        <option value="eu">EU</option>
-        <option value="kr">KR</option>
-      </select>
-      <button onClick={()=> props.getStatData()} type="submit" className="btn btn-danger btn-md">Search</button>
-    </form>
+    <div>
+      <form onSubmit={props.handleFormSubmit}>
+        <input onChange={props.handleInputChange} type="text" value={props.battleTag} id="search-box" autoComplete="off" placeholder="battletag-number"/>
+        <select value={props.region} onChange={props.handleSelectChange}>
+          <option value="us">US</option>
+          <option value="eu">EU</option>
+          <option value="kr">KR</option>
+        </select>
+        <button onClick={()=> props.getStatData()} type="submit" className="btn btn-danger btn-md">Search</button>
+      </form>
+    </div>
   )
 };
 
