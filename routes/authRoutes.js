fe65841e2a12b73b3e2dfafab4993d868ae81938
isaac1104 <isaac-kwon@hotmail.com>
2017-11-14 14:57:47 -0800

    trying to fix

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,6 +4,6 @@ module.exports = app => {
   app.get('/auth/bnet', passport.authenticate('bnet'));
 
   app.get('/auth/bnet/callback', passport.authenticate('bnet', {failureRedirect: '/'}), function(req, res) {
-    res.redirect('/');
+    res.redirect('/log');
   });
 }
--- server.js
+++ server.js
@@ -1,34 +1,30 @@
 const express = require("express");
-const path = require("path");
-const app = express();
 const cookieSession = require("cookie-session");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const routes = require("./routes");
 const passport = require("passport");
-const PORT = process.env.PORT || 5000;
+const path = require("path");
 const cookieKey = process.env.COOKIE_KEY;
-require("./services/passport");
-require("./routes/authRoutes")(app);
+const routes = require("./routes");
 
-// Set up promises with mongoose
-mongoose.Promise = global.Promise;
 // Connect to the Mongo DB
+mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/overwatch", {useMongoClient: true});
+
+//Passport
+require("./services/passport");
+const PORT = process.env.PORT || 5000;
+const app = express();
 
 // Configure body parser for AJAX requests
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-
-//Passport
+app.use(cookieSession({
+  maxAge: 30 * 24 * 60 * 60 * 1000,
+  keys: [cookieKey]
+}));
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(
-  cookieSession({
-    maxAge: 30 * 24 * 60 * 60 * 1000,
-    keys: [cookieKey]
-  })
-);
 
 app.use(routes);
 
@@ -38,8 +34,6 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
-
-
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
