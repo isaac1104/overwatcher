c8e220d23aefcf92dde890aba1bc3924ce5a8479
isaac1104 <isaac-kwon@hotmail.com>
2017-11-15 15:52:41 -0800

    rebuild

--- client/src/components/UserPage/UserPage.js
+++ client/src/components/UserPage/UserPage.js
@@ -97,7 +97,6 @@ class App extends Component {
     return (
       <Router>
         <div>
-          <Route path="/home" component={Home}/>
           <Navbar battleTag={this.state.battleTag} handleFormSubmit={this.handleFormSubmit} handleInputChange={this.handleInputChange} handleSelectChange={this.handleSelectChange} getStatData={this.getStatData} region={this.state.region} greet={this.greet} renderLoadingGif={this.renderLoadingGif()}/>
           {this.renderDataView()}
         </div>
