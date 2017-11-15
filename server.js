1b16f5b59043cd63de6dcc91c393008ed077d10b
isaac1104 <isaac-kwon@hotmail.com>
2017-11-14 21:08:06 -0800

    redirect login route

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.use(passport.session());
 app.use(routes);
 app.get('/auth/bnet', passport.authenticate('bnet'));
 app.get('/auth/bnet/callback', passport.authenticate('bnet', {failureRedirect: '/'}), function(req, res) {
+  console.log(res.json);
   res.redirect("/");
 });
 
