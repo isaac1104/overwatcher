2098f551588a4a44762e43aaa1d5bab4903fe1f8
isaac1104 <isaac-kwon@hotmail.com>
2017-11-14 18:30:29 -0800

    authRoutes changed

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,10 @@
-const passport = require("passport");
-const express = require("express");
-const app = express();
-
-module.exports = app => {
-  app.get('/auth/bnet', passport.authenticate('bnet'));
-
-  app.get('/auth/bnet/callback', passport.authenticate('bnet', {failureRedirect: '/'}), function(req, res) {
-    res.redirect('/log');
-  });
-}
+// const passport = require("passport");
+// const express = require("express");
+// const app = express();
+//
+// module.exports = app => {
+//   app.get('/auth/bnet', passport.authenticate('bnet'));
+//
+//   app.get('/auth/bnet/callback', passport.authenticate('bnet', {failureRedirect: '/'}), function(req, res) {
+//     res.redirect('/log');
+//   });
