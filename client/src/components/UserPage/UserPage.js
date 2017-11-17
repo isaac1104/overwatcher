c069cf411a21b947f8bb644f1f3a66b5b851549e
isaac1104 <isaac-kwon@hotmail.com>
2017-11-16 20:52:54 -0800

    fixing db

--- client/src/components/UserPage/UserPage.js
+++ client/src/components/UserPage/UserPage.js
@@ -19,11 +19,6 @@ class App extends Component {
   componentDidMount() {
     this.loadSavedUsers();
     this.loadSignedInUser();
-    API.saveUsers({username: this.state.signedInUser}).then((res) => {
-      console.log(res.data);
-    }).catch((err) => {
-      console.log(err);
-    });
   }
 
   handleInputChange = (event) => {
@@ -83,11 +78,11 @@ class App extends Component {
           this.setState({renderLoading: false, playerFound: false});
         }
       });
-      // API.saveUsers({username: this.state.battleTag}).then((res) => {
-      //   this.loadSavedUsers();
-      // }).catch((err) => {
-      //   console.log(err);
-      // });
+      API.saveUsers({username: this.state.signedInUser}).then((res) => {
+        this.loadSavedUsers();
+      }).catch((err) => {
+        console.log(err);
+      });
     }
   }
 
--- services/passport.js
+++ services/passport.js
@@ -4,21 +4,19 @@ const BNET_ID = process.env.BNET_ID || "u99r2wbupxtcvr2hp2eds54cwhxdh25k";
 const BNET_SECRET = process.env.BNET_SECRET || "HSFVJdwUFCjvtCbq6wCJBrzKTz9Kyd4n";
 
 passport.serializeUser(function(user, done) {
-    done(null, user);
+  done(null, user);
 });
 
 passport.deserializeUser(function(obj, done) {
-    done(null, obj);
+  done(null, obj);
 });
 
-passport.use(
-  new BnetStrategy(
-    { clientID: BNET_ID,
-      clientSecret: BNET_SECRET,
-      callbackURL: "https://powerful-wildwood-93073.herokuapp.com/auth/bnet/callback"},
-    function(accessToken, refreshToken, profile, done) {
-      process.nextTick(function () {
-        return done(null, profile);
-      });
-    })
-);
+passport.use(new BnetStrategy({
+  clientID: BNET_ID,
+  clientSecret: BNET_SECRET,
+  callbackURL: "https://powerful-wildwood-93073.herokuapp.com/auth/bnet/callback"
+}, function(accessToken, refreshToken, profile, done) {
+  process.nextTick(function() {
+    return done(null, profile);
+  });
+}));
