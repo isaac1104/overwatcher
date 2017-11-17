d81a6b0fcb63a1da9abd98824f9341075610c0a5
isaac1104 <isaac-kwon@hotmail.com>
2017-11-16 21:16:35 -0800

    stream page test

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import {BrowserRouter as Router, Route} from "react-router-dom";
 import Home from "./Home/Home";
 import UserPage from "./UserPage/UserPage";
+import StreamPage from "./StreamPage/StreamPage";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
         <Route exact path="/" component={Home}/>
         <Route exact path="/home" component={Home}/>
         <Route exact path="/user" component={UserPage}/>
+        <Route exact path="/stream" component={StreamPage}/>
       </div>
     </Router>
   )
---
+++ client/src/components/StreamPage/StreamPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const StreamPage = () => {
+  return (
+    <h1>STREAM</h1>
+  )
+};
+
+export default StreamPage;
