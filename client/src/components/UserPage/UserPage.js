a996acc2dfb5aa961a3570acbab38a85fe46a8f9
isaac1104 <isaac-kwon@hotmail.com>
2017-11-16 20:26:19 -0800

    revert change

--- client/src/components/UserPage/UserPage.js
+++ client/src/components/UserPage/UserPage.js
@@ -78,11 +78,11 @@ class App extends Component {
           this.setState({renderLoading: false, playerFound: false});
         }
       });
-      // API.saveUsers({username: this.state.battleTag}).then((res) => {
-      //   this.loadSavedUsers();
-      // }).catch((err) => {
-      //   console.log(err);
-      // });
+      API.saveUsers({username: this.state.battleTag}).then((res) => {
+        this.loadSavedUsers();
+      }).catch((err) => {
+        console.log(err);
+      });
     }
   }
 
@@ -97,11 +97,6 @@ class App extends Component {
   loadSignedInUser = () => {
     API.getSignedInUser().then((res) => {
       this.setState({signedInUser: res.data.battletag});
-      API.saveUsers({username: this.state.signedInUsers}).then((res) => {
-        this.loadSavedUsers();
-      }).catch((err) => {
-        console.log(err);
-      });
     }).catch((err) => {
       console.log(err);
     });
