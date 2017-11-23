5b4d33f1f3c69c020e78023b97f09d967a18da2b
isaac1104 <isaac-kwon@hotmail.com>
2017-11-22 23:31:59 -0800

    thumbnail fetched from data

--- client/src/components/StreamPage/StreamList/StreamList.js
+++ client/src/components/StreamPage/StreamList/StreamList.js
@@ -1,10 +1,14 @@
 import React from "react";
+import template from "url-template";
 
 const StreamList = (props) => {
+
+  let parsedImg = template.parse(props.thumbnail_url).expand({ width: 320, height: 180});
+
   return (
     <div className="container">
       <div className="jumbotron">
-        <img src={props.thumbnail_url} alt="thumbnail"/>
+        <img src={parsedImg} alt="thumbnail"/>
         <h1>{props.title}</h1>
         <h3>Stream Started: {props.started_at}</h3>
         <h5>Viewer Count: {props.viewer_count}</h5>
