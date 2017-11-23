c566a2cc31e85d23c77e69d61eeb8e613bd3a9b1
isaac1104 <isaac-kwon@hotmail.com>
2017-11-22 18:22:44 -0800

    connected twitch api to stream page

--- client/src/components/StreamPage/StreamPage.js
+++ client/src/components/StreamPage/StreamPage.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import Navbar from "./../UserPage/Navbar/Navbar";
+import axios from "axios";
 
-const StreamPage = () => {
-  return (
-    <div>
-      <Navbar/>
-      <h1>STREAM PAGE COMING SOON...</h1>
-    </div>
-  )  
-};
+class StreamPage extends Component {
+
+  componentDidMount() {
+    this.getTwitchData();
+  }
+
+  getTwitchData = () => {
+    axios.get("https://api.twitch.tv/helix/streams?first=20&game_id=488552", {
+      headers: {
+        "Client-ID": "jagnjey4vbrrxhw18toyzzfooz7qc5"
+      }
+    }).then((response) => {
+      console.log(response.data.data);
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar/>
+        <h1>STREAM PAGE COMING SOON...</h1>
+      </div>
+    )
+  }
+}
 
 export default StreamPage;
