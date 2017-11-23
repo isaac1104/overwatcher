4d0bd8018fb195def8f656e37645954d90c87025
isaac1104 <isaac-kwon@hotmail.com>
2017-11-22 20:35:28 -0800

    trying to get thumbnail images for each streams

--- client/src/components/StreamPage/StreamContainer/StreamContainer.js
+++ client/src/components/StreamPage/StreamContainer/StreamContainer.js
@@ -3,12 +3,11 @@ import StreamList from "./../StreamList/StreamList";
 
 const StreamContainer = (props) => {
   console.log(props.result);
-
   return (
     <div>
       {props.result.map((stream) => {
         return (
-          <StreamList title={stream.title} thumbnail_url={stream.thumbnail_url} started_at={stream.started_at} viewer_count={stream.viewer_count} id={stream.id}/>
+          <StreamList title={stream.title} thumbnail_url={stream.thumbnail_url} started_at={stream.started_at} viewer_count={stream.viewer_count} id={stream.id} width={320} height={180}/>
         )
       })}
     </div>
--- client/src/components/StreamPage/StreamList/StreamList.js
+++ client/src/components/StreamPage/StreamList/StreamList.js
@@ -2,11 +2,13 @@ import React from "react";
 
 const StreamList = (props) => {
   return (
-    <div className="jumbotron">
-      <img src={props.thumbnail_url} alt="thumbnail"/>
-      <h1>{props.title}</h1>
-      <h3>Stream Started: {props.started_at}</h3>
-      <h5>Viewer Count: {props.viewer_count}</h5>
+    <div className="container"> 
+      <div className="jumbotron">
+        <img src={props.thumbnail_url} alt="thumbnail"/>
+        <h1>{props.title}</h1>
+        <h3>Stream Started: {props.started_at}</h3>
+        <h5>Viewer Count: {props.viewer_count}</h5>
+      </div>
     </div>
   )
 };
