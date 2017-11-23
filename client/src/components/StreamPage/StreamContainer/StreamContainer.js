cbe26d6f75e9f2e73638a0c40b596f7575049a59
isaac1104 <isaac-kwon@hotmail.com>
2017-11-22 19:56:54 -0800

    results mapping in stream page

--- client/src/components/StreamPage/StreamContainer/StreamContainer.js
+++ client/src/components/StreamPage/StreamContainer/StreamContainer.js
@@ -1,14 +1,14 @@
-import _ from "lodash";
 import React from "react";
 import StreamList from "./../StreamList/StreamList";
 
 const StreamContainer = (props) => {
   console.log(props.result);
+
   return (
     <div>
       {props.result.map((stream) => {
         return (
-          <StreamList result={props.result}/>
+          <StreamList title={stream.title} thumbnail_url={stream.thumbnail_url} started_at={stream.started_at} viewer_count={stream.viewer_count} id={stream.id}/>
         )
       })}
     </div>
--- client/src/components/StreamPage/StreamList/StreamList.js
+++ client/src/components/StreamPage/StreamList/StreamList.js
@@ -3,10 +3,10 @@ import React from "react";
 const StreamList = (props) => {
   return (
     <div className="jumbotron">
-      <img src={props.result.thumbnail_url} alt="thumbnail"/>
-      <h1>{props.result.title}</h1>
-      <h3>{props.result.started_at}</h3>
-      <h5>{props.result.viewer_count}</h5>
+      <img src={props.thumbnail_url} alt="thumbnail"/>
+      <h1>{props.title}</h1>
+      <h3>Stream Started: {props.started_at}</h3>
+      <h5>Viewer Count: {props.viewer_count}</h5>
     </div>
   )
 };
