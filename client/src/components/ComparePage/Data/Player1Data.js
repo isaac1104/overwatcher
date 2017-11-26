27ccb2918167ff8968210ed60a472072da720087
isaac1104 <isaac-kwon@hotmail.com>
2017-11-25 20:10:30 -0800

    data changed on compare page

--- client/src/components/ComparePage/Data/Player1Data.js
+++ client/src/components/ComparePage/Data/Player1Data.js
@@ -78,7 +78,7 @@ const Player1Data = (props) => {
             <h5><strong>Win Rate</strong></h5>
             <hr/>
             <h6>{((competitiveStats.games.won / competitiveStats.games.played) * 100).toFixed(0)}%</h6>
-            <h6>{competitiveStats.games.won} W {allHeroes.average.gamesTied} D {allHeroes.average.gamesLost} L</h6>
+            <h6>{competitiveStats.games.won}W {allHeroes.average.gamesTied}D {allHeroes.average.gamesLost}L</h6>
           </div>
           <div className="col-md-4">
             <h5><strong>Total Games Won</strong></h5>
