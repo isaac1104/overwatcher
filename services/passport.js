32aa4ee4dfc497369e12ee865dca968e1cdaed50
isaac1104 <isaac-kwon@hotmail.com>
2017-12-05 18:32:21 -0800

    mongodb connect attpempt

--- services/passport.js
+++ services/passport.js
@@ -2,21 +2,52 @@ const passport = require("passport");
 const BnetStrategy = require('passport-bnet').Strategy;
 const BNET_ID = process.env.BNET_ID || "u99r2wbupxtcvr2hp2eds54cwhxdh25k";
 const BNET_SECRET = process.env.BNET_SECRET || "HSFVJdwUFCjvtCbq6wCJBrzKTz9Kyd4n";
+const mongoose = require("mongoose");
+const User = mongoose.model("users");
 
-passport.serializeUser(function(user, done) {
-  done(null, user);
+// passport.serializeUser(function(user, done) {
+//   done(null, user);
+// });
+//
+// passport.deserializeUser(function(obj, done) {
+//   done(null, obj);
+// });
+
+// passport.use(new BnetStrategy({
+//   clientID: BNET_ID,
+//   clientSecret: BNET_SECRET,
+//   callbackURL: "https://powerful-wildwood-93073.herokuapp.com/auth/bnet/callback"
+// }, function(accessToken, refreshToken, profile, done) {
+//   process.nextTick(function() {
+//     return done(null, profile);
+//   });
+// }));
+
+passport.serializeUser( (user, done) => {
+    done(null, user.id);
 });
 
-passport.deserializeUser(function(obj, done) {
-  done(null, obj);
-});
-
-passport.use(new BnetStrategy({
-  clientID: BNET_ID,
-  clientSecret: BNET_SECRET,
-  callbackURL: "https://powerful-wildwood-93073.herokuapp.com/auth/bnet/callback"
-}, function(accessToken, refreshToken, profile, done) {
-  process.nextTick(function() {
-    return done(null, profile);
+passport.deserializeUser((obj, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
   });
-}));
+});
+
+passport.use(
+  new BnetStrategy(
+    {
+      clientID: BNET_ID,
+      clientSecret: BNET_SECRET,
+      callbackURL: "/auth/bnet/callback",
+      proxy: true
+    },
+    async(accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ bnetId: profile.id });
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+      const user = await new User({ bnetId: profile.id }).save();
+      done(null, user);
+    }
+  )
+);
