58cebd7f825312d91cbff3997ed8f454bb190e92
isaac1104 <isaac-kwon@hotmail.com>
2017-12-05 18:13:47 -0800

    reverse change

--- models/user.js
+++ models/user.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const UserSchema = new Schema({
-  bnetId: String
+    username: {
+    type: String,
+    required: true
+  }
 });
 
 const User = mongoose.model("User", UserSchema);
+module.exports = User;
--- services/passport.js
+++ services/passport.js
@@ -6,30 +6,40 @@ const BNET_SECRET = process.env.BNET_SECRET || "HSFVJdwUFCjvtCbq6wCJBrzKTz9Kyd4n
 const User = mongoose.model("User");
 
 passport.serializeUser((user, done) => {
-  done(null, user.id);
+  done(null, user);
 });
 
 passport.deserializeUser((obj, done) => {
   User.findById(id).then(user => {
-    done(null, user);
+    done(null, obj);
   });
 });
 
-passport.use(
-  new BnetStrategy(
-    {
-      clientID: BNET_ID,
-      clientSecret: BNET_SECRET,
-      callbackURL: "/auth/bnet/callback",
-      proxy: true
-    },
-    async (accessToken, refreshToken, profile, done) => {
-      const existingUser = await User.findOne({ bnetId: profile.id });
-      if (existingUser) {
-        return done(null, existingUser);
-      }
-      const user = await new User({ bnetId: profile.id }).save();
-      done(null, user);
-    }
-  )
-);
+// passport.use(
+//   new BnetStrategy(
+//     {
+//       clientID: BNET_ID,
+//       clientSecret: BNET_SECRET,
+//       callbackURL: "/auth/bnet/callback",
+//       proxy: true
+//     },
+//     async (accessToken, refreshToken, profile, done) => {
+//       const existingUser = await User.findOne({ bnetId: profile.id });
+//       if (existingUser) {
+//         return done(null, existingUser);
+//       }
+//       const user = await new User({ bnetId: profile.id }).save();
+//       done(null, user);
+//     }
+//   )
+// );
+
+passport.use(new BnetStrategy({
+  clientID: BNET_ID,
+  clientSecret: BNET_SECRET,
+  callbackURL: "https://powerful-wildwood-93073.herokuapp.com/auth/bnet/callback"
+}, function(accessToken, refreshToken, profile, done) {
+  process.nextTick(function() {
+    return done(null, profile);
+  });
+}));
