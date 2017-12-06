abb9dee2057eb0cb6e318d175aca08ce80371a4e
isaac1104 <isaac-kwon@hotmail.com>
2017-12-06 12:58:09 -0800

    current user route directory changed

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -42,7 +42,7 @@ export const initStatsFetch = () => {
 }
 
 export const fetchSignedInBattleTag = () => async (dispatch) => {
-  const request = await axios.get("https://powerful-wildwood-93073.herokuapp.com/log");
+  const request = await axios.get("https://powerful-wildwood-93073.herokuapp.com/api/current_user");
   const { data } = request;
   dispatch({ type: FETCH_SIGNED_IN_BATTLETAG, payload: data });
 }
