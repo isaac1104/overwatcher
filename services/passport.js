755e2b10f4dc61914c3fe99bf88ea3b5bc40b7eb
isaac1104 <isaac-kwon@hotmail.com>
2017-12-05 18:17:38 -0800

    reverse change to passport

--- services/passport.js
+++ services/passport.js
@@ -1,38 +1,15 @@
 const passport = require("passport");
-const mongoose = require("mongoose");
 const BnetStrategy = require('passport-bnet').Strategy;
 const BNET_ID = process.env.BNET_ID || "u99r2wbupxtcvr2hp2eds54cwhxdh25k";
 const BNET_SECRET = process.env.BNET_SECRET || "HSFVJdwUFCjvtCbq6wCJBrzKTz9Kyd4n";
-const User = mongoose.model("User");
 
-passport.serializeUser((user, done) => {
+passport.serializeUser(function(user, done) {
   done(null, user);
 });
 
-passport.deserializeUser((obj, done) => {
-  User.findById(id).then(user => {
-    done(null, obj);
-  });
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
 });
-
-// passport.use(
-//   new BnetStrategy(
-//     {
-//       clientID: BNET_ID,
-//       clientSecret: BNET_SECRET,
-//       callbackURL: "/auth/bnet/callback",
-//       proxy: true
-//     },
-//     async (accessToken, refreshToken, profile, done) => {
-//       const existingUser = await User.findOne({ bnetId: profile.id });
-//       if (existingUser) {
-//         return done(null, existingUser);
-//       }
-//       const user = await new User({ bnetId: profile.id }).save();
-//       done(null, user);
-//     }
-//   )
-// );
 
 passport.use(new BnetStrategy({
   clientID: BNET_ID,
