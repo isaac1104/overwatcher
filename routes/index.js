503807335af024f0cae10a5628f5ffe1b0d3b35e
isaac1104 <isaac-kwon@hotmail.com>
2017-12-07 13:57:46 -0800

    Building for production

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-// const path = require("path");
-// const router = require("express").Router();
-// const apiRoutes = require("./api");
-// const authRoutes = require("./authRoutes");
-//
-// router.use("/api", apiRoutes);
-//
-// module.exports = router;
