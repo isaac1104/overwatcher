feffd119721e217980364cf06d1f730e23d62e45
isaac1104 <isaac-kwon@hotmail.com>
2017-12-07 11:25:36 -0800

    Building for production

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.get("/logout", (req, res) => {
   res.redirect("/");
 });
 
-app.post("/api/favorite", (req, res) => {
+app.get("/api/favorite", (req, res) => {
   User.update({ "bnetId": "79838992" }, { $push: { test: "test" }});
   res.json(req.user);
   console.log(req);
