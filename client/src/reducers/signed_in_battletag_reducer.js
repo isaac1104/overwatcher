0312170ae47c33635eae06a0070650d77257ca6a
isaac1104 <isaac-kwon@hotmail.com>
2017-12-13 15:25:02 -0800

    [debug]User state debugging

--- client/src/reducers/signed_in_battletag_reducer.js
+++ client/src/reducers/signed_in_battletag_reducer.js
@@ -5,7 +5,10 @@ const initialState = {};
 function signedInBattleTag(state = initialState, action) {
   switch (action.type) {
     case FETCH_SIGNED_IN_BATTLETAG:
-      return action.payload;
+      return {
+      ...state,
+      data: action.payload
+    };
     default:
       return state;
   }
