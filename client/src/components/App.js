3c7a9e3563784522e35a59933dc95792450f25da
isaac1104 <isaac-kwon@hotmail.com>
2017-12-13 17:11:15 -0800

    [refactor]Refactored App.js to fetch battletag

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./Home/Home";
 import Navbar from "./../containers/Navbar";
@@ -8,10 +8,16 @@ import ComparePage from "./ComparePage/ComparePage";
 import FavoritePage from "./FavoritePage/FavoritePage";
 import PlayerNotFound from "./PlayerNotFound/PlayerNotFound";
 import DataView from "./../containers/DataView";
+import { connect } from "react-redux";
+import * as actions from "./../actions";
 
-const App = () => {
+class App extends Component {
 
-  const renderNavbar = () => {
+  componentDidMount() {
+    this.props.fetchSignedInBattleTag();
+  }
+
+  renderNavbar = () => {
     if (window.location.href === `${window.location.origin}/` || window.location.href === `${window.location.origin}/login`) {
       return <div></div>
     } else {
@@ -19,21 +25,27 @@ const App = () => {
     }
   }
 
-  return (
-    <Router>
-      <div>
-        {renderNavbar()}
-        <Route exact path="/" component={Home}/>
-        <Route exact path="/login" component={Home}/>
-        <Route exact path="/user/search" component={UserPage}/>
-        <Route exact path="/user/notfound" component={PlayerNotFound}/>
-        <Route exact path="/user/search/:player" component={DataView}/>
-        <Route exact path="/user/stream" component={StreamPage}/>
-        <Route exact path="/user/compare" component={ComparePage}/>
-        <Route exact path="/user/favorite" component={FavoritePage}/>
-      </div>
-    </Router>
-  );
+  render() {
+    return (
+      <Router>
+        <div>
+          {this.renderNavbar()}
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/login" component={Home}/>
+          <Route exact path="/user/search" component={UserPage}/>
+          <Route exact path="/user/notfound" component={PlayerNotFound}/>
+          <Route exact path="/user/search/:player" component={DataView}/>
+          <Route exact path="/user/stream" component={StreamPage}/>
+          <Route exact path="/user/compare" component={ComparePage}/>
+          <Route exact path="/user/favorite" component={FavoritePage}/>
+        </div>
+      </Router>
+    );
+  }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return { user: state.user }
+}
+
+export default connect(mapStateToProps, actions)(App);
