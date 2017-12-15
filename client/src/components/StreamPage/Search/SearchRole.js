6de72df1e0bc54084820449c851d8640389a5a46
isaac1104 <isaac-kwon@hotmail.com>
2017-12-14 20:12:32 -0800

    [design]Filter button restyled on streampage

--- client/src/components/StreamPage/Search/SearchRole.js
+++ client/src/components/StreamPage/Search/SearchRole.js
@@ -1,14 +1,101 @@
 import React from "react";
+import { Button, Icon, Image } from "semantic-ui-react";
 
 const SearchHeroes = (props) => {
   return (
     <div className="streamer-hero-search-container text-center">
       <div>
         <h5>Filter By Role</h5>
-        <img onClick={() => props.filterStream("Support")} src="/images/role-icons/support-icon.png" alt="support-icon" className="img-responsive role-icons"/>
-        <img onClick={() => props.filterStream("Offense")} src="/images/role-icons/offense-icon.png" alt="offense-icon" className="img-responsive role-icons"/>
-        <img onClick={() => props.filterStream("Defense")} src="/images/role-icons/defense-icon.png" alt="defense-icon" className="img-responsive role-icons"/>
-        <img onClick={() => props.filterStream("Tank")} src="/images/role-icons/tank-icon.png" alt="tank-icon" className="img-responsive role-icons"/>
+        {/* Support */}
+        <Button
+          onClick={() => props.filterStream("Support")}
+          animated="vertical"
+          basic
+          inverted
+          color="teal"
+        >
+          <Button.Content visible>
+            <Image
+              src="/images/role-icons/support-icon.png"
+              size="mini"
+            />
+          </Button.Content>
+          <Button.Content hidden>
+            SUPPORT
+          </Button.Content>
+        </Button>
+        {/* Offense */}
+        <Button
+          onClick={() => props.filterStream("Offense")}
+          animated="vertical"
+          basic
+          inverted
+          color="red"
+        >
+          <Button.Content visible>
+            <Image
+              src="/images/role-icons/offense-icon.png"
+              size="mini"
+            />
+          </Button.Content>
+          <Button.Content hidden>
+            OFFENSE
+          </Button.Content>
+        </Button>
+        {/* Defense */}
+        <Button
+          onClick={() => props.filterStream("Defense")}
+          animated="vertical"
+          basic
+          inverted
+          color="blue"
+        >
+          <Button.Content visible>
+            <Image
+              src="/images/role-icons/defense-icon.png"
+              size="mini"
+            />
+          </Button.Content>
+          <Button.Content hidden>
+            DEFENSE
+          </Button.Content>
+        </Button>
+        {/* Tank */}
+        <Button
+          onClick={() => props.filterStream("Tank")}
+          animated="vertical"
+          basic
+          inverted
+          color="purple"
+        >
+          <Button.Content visible>
+            <Image
+              src="/images/role-icons/tank-icon.png"
+              size="mini"
+            />
+          </Button.Content>
+          <Button.Content hidden>
+            TANK
+          </Button.Content>
+        </Button>
+        {/* All */}
+        <Button
+          onClick={() => props.filterStream("All")}
+          animated="vertical"
+          basic
+          inverted
+          color="orange"
+        >
+          <Button.Content visible>
+            <Image
+              src="/images/misc/owlogo.svg"
+              size="mini"
+            />
+          </Button.Content>
+          <Button.Content hidden>
+            ALL
+          </Button.Content>
+        </Button>
       </div>
     </div>
   )
