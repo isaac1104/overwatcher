722bd1dd974d40698780f5d907632baae77a8077
isaac1104 <isaac-kwon@hotmail.com>
2017-12-20 22:46:16 -0800

    Building for production

--- client/src/components/ComparePage/NotFound/NotFound.js
+++ client/src/components/ComparePage/NotFound/NotFound.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { Image } from "semantic-ui-react";
 import "./NotFound.css";
 
 const NotFound = () => {
   return (
     <div className="container">
       <div className="jumbotron text-center not-found-header">
+        <Image
+          src="/images/misc/reaper.png"
+          style={{width: "10rem"}}
+          centered
+        />
         <h1>Error: One of more players not found!</h1>
         <h3>Please be sure to input two valid Battletags</h3>
         <h5>Two compared players must have played at least one competitive game</h5>
